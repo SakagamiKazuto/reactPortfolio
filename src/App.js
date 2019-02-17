aa906cbe1c33b42c2956e7ad539e3746ac80164b
SakagamiKazuto <carpwin115@icloud.com>
2019-02-18 00:37:11 +0900

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDoListItem from "./ToDoListItem.js"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    todoList: [],
+    all_id: 1
+    };
+this.idincrement = this.idincrement.bind(this);
+}
+
+//
+idincrement() {
+  this.setState(prevState =>{
+      return {
+          all_id: prevState.all_id + 1
+      };
+  });
+}
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form
+          className="App-form"
+          onSubmit={e => {
+            // formのデフォルトのイベントをキャンセル
+            e.preventDefault();
+
+            this.idincrement();
+
+            // idがtitleのElementを取得
+            const titleElement = e.target.elements["title"]
+            // idがdescriptionのElementを取得
+            const descriptionElement = e.target.elements["description"];
+
+            // todoList stateに追加
+            this.setState(
+              {
+                todoList: this.state.todoList.concat({
+                  title: titleElement.value,
+                  description: descriptionElement.value,
+                  todo_list_id: this.state.all_id
+                })
+              },
+              // stateの変更後に入力した値を空にする
+              // e.preventDefaultでイベントをキャンセルしたため、
+              // これがないと値が残ります
+              () => {
+                titleElement.value = "";
+                descriptionElement.value = "";
+              }
+            )
+          }}
+        >
+          <div>
+            <input
+              id="title"
+              placeholder="title"
+            />
+            <textarea
+              id="description"
+              placeholder="description"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+            >
+              登録
+            </button>
+          </div>
+        </form>
+        <div>
+        {/* todoList配列の要素数分ToDoListItemコンポーネントを展開 */}
+        {this.state.todoList.map(todo => (
+
+            <ToDoListItem
+              todo_list_id={todo.todo_list_id}
+              key={todo.title}
+              title={todo.title}
+              description={todo.description} 
+
+              // クリックされたItemをtodoList stateから削除
+              onClick={() => {
+                this.setState({
+                  todoList: this.state.todoList.filter(x => x !== todo)
+                })
+              }}               
+            />
+          ))}
+        </div>
       </div>
     );
   }
